483bb3af332b1a06f937aae6350654a6108aeadc
iga waclawska <iga.waclawska@gmail.com>
2023-11-04 18:42:05 +0100

    rename GetStartedPage to LoginOrSignupPage

--- Group10_Urban_Oasis/pages/LoginOrSignUp.js
+++ Group10_Urban_Oasis/pages/LoginOrSignUp.js
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-export const GetStartedPage = () => {
+export const LoginOrSignupPage = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Get Started Page</Text>
+      <Text>Log in or Sign up</Text>
       {/* button component added temporarily for simplicity, can be exchanged with a Pressable later */}
-      <Button title="Log in" />
-      <Button title="Register" />
+      <Button title="Continue with Email" />
     </View>
   );
 };
