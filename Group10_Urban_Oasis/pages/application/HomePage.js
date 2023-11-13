0258e2c40583d4b6c88db04f3f2afb5afd392624
faha92 <fabianharlang@icloud.com>
2023-11-13 16:23:47 +0100

    map import

--- Group10_Urban_Oasis/pages/application/HomePage.js
+++ Group10_Urban_Oasis/pages/application/HomePage.js
@@ -1,6 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+// map import goes here // 
+
+
+
 export const HomePage = () => {
   return (
     <View style={styles.container}>
