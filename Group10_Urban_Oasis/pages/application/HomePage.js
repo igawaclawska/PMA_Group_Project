285b78c40a81b96e67b5441b5544ae7d9395c135
faha92 <fabianharlang@icloud.com>
2023-11-15 13:24:41 +0100

    naive map implementation

--- Group10_Urban_Oasis/pages/application/HomePage.js
+++ Group10_Urban_Oasis/pages/application/HomePage.js
@@ -1,16 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MapView from 'react-native-maps';
 
 // map import goes here // 
 
+const defaultLoactions = [
+{ title: 'test locatation',
+location: {
 
+  "latitude": 10.453921831373197,
+  "longitude": 55.61089535500283,
+}
+
+    
+, description: 'this is just a test'
+}
+
+]
 
 export const HomePage = () => {
+
+const onRegionChange = (region) =>{
+
+  console.log(region);
+}
+
+
+
   return (
-    <View style={styles.container}>
+    
+   <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Map view goes Here</Text>
-    </View>
+      <MapView style={styles.map} initialRegion={{ latitude: 55.61089535500283, longitude: 10.453921831373197  }} onRegionChange={onRegionChange}></MapView>
+  </View>
+  
   );
 };
 
@@ -20,5 +44,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+   
   },
+  map: {
+   
+    width: '100%',
+    height: '100%'
+  }
 });
