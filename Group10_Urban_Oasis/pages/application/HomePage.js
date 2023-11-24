cf7209a6f3f02f9469cf0858fb4d079745a7d02e
faha92 <fabianharlang@icloud.com>
2023-11-24 16:11:08 +0100

    markers added

--- Group10_Urban_Oasis/pages/application/HomePage.js
+++ Group10_Urban_Oasis/pages/application/HomePage.js
@@ -1,26 +1,257 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import MapView from 'react-native-maps';
+import { useState } from "react";
+import MapView, { Marker } from 'react-native-maps';
 
 // map import goes here // 
 
-const defaultLoactions = [
-{ title: 'test locatation',
-location: {
+//map style vector 
 
-  "latitude": 10.453921831373197,
-  "longitude": 55.61089535500283,
-}
+const mapJson = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#ffeb3b"
+      },
+      {
+        "weight": 3
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#1f0038"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+];
 
-, description: 'this is just a test'
-}
-
-]
+// default markers
+let defaultLocations = [
+  {
+    title: "First",
+    location: {
+      latitude: 55.60866491013769,
+      longitude: 12.5911277895021
+    },
+    description: "Hidden Location #1"
+  },
+  {
+    title: "Second",
+    location: {
+      latitude: 55.813353748065204,
+      longitude: 12.34104207156517
+    },
+    description: "Hidden Location #2"
+  }
+];
 
 export const HomePage = () => {
 
+  const [draggableMarkerCoord, setDraggableMarkerCoord] = useState({ latitude: 37.78825,
+    longitude: -122.4324});
+
+ 
+    const showDefaultLocations = () => {
+      return defaultLocations.map((item, index) => {
+        return (
+          <Marker 
+            key={index}
+            coordinate={item.location}
+            title={item.title}
+            description={item.description}
+            pinColor={'#FFC0CB'}
+            
+          />
+        )
+      });
+    };
+  
 const onRegionChange = (region) =>{
 
+
+  // console log region change
   console.log(region);
 }
 
@@ -31,7 +262,26 @@ const onRegionChange = (region) =>{
    <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>Map view goes Here</Text> 
-      <MapView style={styles.map}initialRegion={{ latitude: 55.58376336969238, latitudeDelta: 2.1949835082157563, longitude: 10.4347135309621, longitudeDelta: 2.4010375513883844} } onRegionChange={onRegionChange}></MapView>
+      <MapView style={styles.map}
+      onRegionChange={onRegionChange}
+      initialRegion={{
+      latitude: 55.71679184033459,
+      latitudeDelta: 0.8060190506549958,
+      longitude: 12.374072260726619,
+      longitudeDelta: 0.884737209682612,}} 
+      customMapStyle={mapJson}
+>
+   
+    
+    {showDefaultLocations()}
+
+    <Marker
+    draggable
+    coordinate={draggableMarkerCoord}
+    onDragEnd={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}
+    />
+
+</MapView>
   </View>
   
   );
@@ -48,6 +298,7 @@ const styles = StyleSheet.create({
   map: {
    
     width: '100%',
-    height: '100%'
+    height: '100%',
+    zIndex: 1
   }
 });
